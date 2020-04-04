b287a0ae8106bf08efb0190ebfd8909b1f251d80
prodeveloper <dany.ourapps99@gmail.com>
2020-04-04 18:54:49 +1100

    Navigate through screens added

--- App.js
+++ App.js
@@ -1,19 +1,48 @@
 import React from 'react';
-import { StyleSheet, Text, View } from 'react-native';
+import { StyleSheet, Text, View, Button } from 'react-native';
+import { NavigationContainer } from '@react-navigation/native'
+import { createStackNavigator } from '@react-navigation/stack'
 
-export default function App() {
-  return (
-    <View style={styles.container}>
-      <Text>Open up App.js to start working on your app!</Text>
-    </View>
-  );
+
+const Stack = createStackNavigator()
+export default class App extends React.Component {
+  render() {
+    return (
+      <NavigationContainer>
+        <Stack.Navigator>
+          <Stack.Screen name="Home" component={Home} />
+          <Stack.Screen name="SignIn" component={SignIn} />
+          <Stack.Screen name="SignUp" component={SignUp} />
+        </Stack.Navigator>
+      </NavigationContainer>
+    );
+  }
 }
 
-const styles = StyleSheet.create({
-  container: {
-    flex: 1,
-    backgroundColor: '#fff',
-    alignItems: 'center',
-    justifyContent: 'center',
-  },
-});
+class Home extends React.Component {
+  render() {
+    return (
+      <View>
+        <Button title="Go to Sign In" onPress={() => this.props.navigation.navigate('SignIn')} />
+      </View>
+    );
+  }
+}
+
+class SignIn extends React.Component {
+  render() {
+    return (
+      <View>
+        <Button title="Sign Up" onPress={() => this.props.navigation.replace('SignUp')} />
+      </View>
+    );
+  }
+}
+
+class SignUp extends React.Component {
+  render() {
+    return (
+      <Text>HOME</Text>
+    );
+  }
+}
