8ab3a1cade8344df7ac393636c955dc4926f4bfb
prodeveloper <dany.ourapps99@gmail.com>
2020-04-04 21:40:02 +1100

    added form and firebase

--- App.js
+++ App.js
@@ -1,8 +1,8 @@
 import React from 'react';
-import { StyleSheet, Text, View, Button } from 'react-native';
+import { StyleSheet, Text, View, Button, TextInput, TouchableOpacity } from 'react-native';
 import { NavigationContainer } from '@react-navigation/native'
 import { createStackNavigator } from '@react-navigation/stack'
-
+import * as firebase from 'firebase'
 
 const Stack = createStackNavigator()
 export default class App extends React.Component {
@@ -62,10 +62,49 @@ class Home extends React.Component {
 }
 
 class SignIn extends React.Component {
+
+  constructor() {
+    super()
+    this.state = {
+      name: '',
+      email: '',
+      pwd: '',
+    }
+  }
+
+  signUp = () => {
+    const { name, email, pwd } = this.state;
+
+  }
   render() {
     return (
       <View>
-        <Button title="Sign Up" onPress={() => this.props.navigation.replace('SignUp')} />
+        <View style={styles.form}>
+          <TextInput placeholder="Name"
+            placeholderTextColor="#D0CBD4"
+            keyboardType="default"
+            autoCapitalize={'words'}
+            style={styles.input}
+            onChangeText={name => this.setState({ name })} />
+          <TextInput
+            placeholder="Email"
+            placeholderTextColor="#D0CBD4"
+            keyboardType="email-address"
+            autoCapitalize={'none'}
+            style={styles.input}
+            onChangeText={email => this.setState({ email })} />
+          <TextInput placeholder="Password"
+            placeholderTextColor="#D0CBD4"
+            keyboardType="default"
+            secureTextEntry={true}
+            autoCapitalize={'words'}
+            style={styles.input}
+            onChangeText={pwd => this.setState({ pwd })} />
+        </View>
+        <TouchableOpacity onPress={() => this.signUp()}>
+          <Text style={styles.submitText}>Submit</Text>
+        </TouchableOpacity>
+        <Button title="Create an account?" onPress={() => this.props.navigation.replace('SignUp')} />
       </View>
     );
   }
@@ -78,3 +117,46 @@ class SignUp extends React.Component {
     );
   }
 }
+
+
+const styles = StyleSheet.create({
+  container: {
+    flex: 1,
+  },
+  header: {
+    backgroundColor: '#1875D2',
+    height: 130,
+  },
+  headerText: {
+    display: 'flex',
+    flex: 1,
+    alignSelf: 'flex-start',
+    paddingVertical: 60,
+    paddingHorizontal: 20,
+    fontSize: 38,
+    color: '#ffffff',
+    fontWeight: 'bold'
+  },
+  input: {
+    borderBottomWidth: 1,
+    borderColor: '#D0CBD4',
+    borderRadius: 10,
+    paddingVertical: 10,
+    paddingHorizontal: 9,
+    fontSize: 20,
+    marginBottom: 20,
+    color: '#1C1D21'
+  },
+  form: {
+    marginTop: 16,
+    marginHorizontal: 8,
+  },
+  submitText: {
+    backgroundColor: '#0C60FF',
+    color: '#ffffff',
+    marginHorizontal: 30,
+    paddingVertical: 20,
+    borderRadius: 30,
+    textAlign: 'center',
+  }
+})
